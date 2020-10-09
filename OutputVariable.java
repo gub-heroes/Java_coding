02f764d2407a6635df1bb797082dc6b1e6d255c4
Fahim Al Huq <60819038+Fahim2000@users.noreply.github.com>
2020-10-09 19:47:22 +0600

    Add files via upload

---
+++ OutputVariable.java
@@ -0,0 +1,19 @@
+
+package outputvariable;
+
+import java.util.Scanner;
+
+
+public class OutputVariable {
+
+   
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int number;
+      System.out.print("Enter the number : ");
+      number = input.nextInt();
+      System.out.println("The number is " +number);
+    
+    }
+    
+}
